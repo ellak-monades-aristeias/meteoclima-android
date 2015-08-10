15b034cdd08909a00eb98cc4bd9cdeae46dc23de
spyrosel <spyros.eleftheriadis@gmail.com>
2015-08-10 14:40:01 +0300

    first test commit

--- app/src/main/java/gr/qpc/meteoclimaandroid/MeteoclimaMainActivity.java
+++ app/src/main/java/gr/qpc/meteoclimaandroid/MeteoclimaMainActivity.java
@@ -11,6 +11,7 @@ public class MeteoclimaMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meteoclima_main);
+        //test for git
     }
 
     @Override
